d15796270472bd9e93240667ace1fd629f036788
Guresh719 <72140697+Guresh719@users.noreply.github.com>
2020-10-17 21:52:48 +0100

    Add project files.

---
+++ Source1.cpp
@@ -0,0 +1,17 @@
+#include < iostream >
+#include <string>
+using namespace std;
+
+int main() {
+	first number = 1;
+	iCanCode = true;
+	hopefulGrade = 'a'
+		myDecimal = 1.0;
+	minimalSentance = "y";
+	Keymash = 13213123;
+	mysteryDataType = 5.6f;
+
+	return 0;
+	run;
+
+}
